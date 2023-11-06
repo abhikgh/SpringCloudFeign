95cb850de5e6117ae4d9e0584fb1e758518b4ae6
Abhik <abhikgh.work@gmail.com>
2023-11-06 12:49:41 +0530

    updates

--- src/main/java/com/example/SpringCloudFeign/service/RestFeignService.java
+++ src/main/java/com/example/SpringCloudFeign/service/RestFeignService.java
@@ -7,10 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "SpringBootREST2", url = "http://localhost:9050/orders/v1", configuration = CloudConfig.class, fallback = FallbackService.class)
+@FeignClient(value = "SpringBootREST2", url = "http://localhost:9050/orders/v1", configuration = CloudConfig.class)
 public interface RestFeignService {
 
     @GetMapping(value = "/getMovieDetails/{movieId}", produces = MediaType.APPLICATION_JSON_VALUE)
     Movie getMovieDetailsFeign(@PathVariable("movieId") Integer movieId);
 
 }
+
